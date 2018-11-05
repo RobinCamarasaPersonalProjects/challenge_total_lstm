a8ebb520794fcb82d8836b80ecaccaf8cc886a07
RobinCamarasa <robin.camarasa@etu.emse.fr>
2018-11-05 17:28:47 +0100

    Fixed argument bug

--- process/process.sh
+++ process/process.sh
@@ -5,4 +5,5 @@ n=`expr $(ls ../../results_$1 | sort -n | tail -n1) + 1`
 echo "$1; $n;" >> ../../dashboard.csv
 
 mkdir ../../results_$1/$n
-python -W ignore process.py $n $1 $2
+cp -R ../../src/ ../../results_$1/$n/src/
+python -W ignore process.py $n $2 $1
